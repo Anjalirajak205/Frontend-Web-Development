1a7072955ebe640b74355aafbda921d3e6ac4a68
Anjalirajak205 <anjalirajak205@gmail.com>
2025-08-25 22:44:01 +0530

    25 august

---
+++ JavaSript/index.js
@@ -0,0 +1,46 @@
+// var a = 10
+// console.log(a)
+
+// let a=10
+// a=20
+// const c=10
+
+// console.log(5==5)  //== sirf value check kerta hai aur ===
+// // value aur data dono compare kerta hai aur == assign kerta hai value
+// console.log(5==true)
+// console.log({}=={})
+// console.log(5=='5')
+// console.log(5==='5');
+
+//difference null and undefined
+
+//var a = null
+
+//let a = 100
+//let to=a==100
+//console.log(to);
+
+// let useName="Anjali"
+// let lastname="Rajak"
+// console.log(`${useName} ${lastname}`);
+// console.log(useName+lastname)
+// console.log(useName+" "+lastname)
+
+
+//if-else in js
+//let a=true
+//let a=5
+
+//if(a)
+//if(0)
+// if(a=true){
+//   console.log("hello");
+// }
+// else{
+//     console.log("hii");
+// }
+let a=false
+a?console.log("hii"):console.log("byee");
+console.log(1+1);
+console.log('1'+2)
+console.log('1'-2)
