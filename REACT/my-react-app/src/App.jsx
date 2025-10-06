c4e0d01270aa2a81f0a4c3de5ae2fffd4f0938ff
Anjalirajak205 <anjalirajak205@gmail.com>
2025-10-06 19:16:37 +0530

    06 october

--- REACT/my-react-app/src/App.jsx
+++ REACT/my-react-app/src/App.jsx
@@ -68,32 +68,109 @@ export default App */
 
 // 23 september
  
-import React, {useState} from "react"
-import Home from './Home'
-const App = () =>{
+// import React, {useState} from "react"
+// import Home from './Home'
+//const App = () =>{
  // console.log("hii");
-  let [state,SetState]= useState(0)
-  function fun1(){
+  // let [state,SetState]= useState(0)
+  // function fun1(){
     //SetState(state+1)
       //SetState("green")
-      if(state=='red'){
-        SetState("gold")
-      }else{
-        SetState("red")
-      }
-  }
-   return (
+  //     if(state=='red'){
+  //       SetState("gold")
+  //     }else{
+  //       SetState("red")
+  //     }
+  // }
+  //  return (
     // <div style={{backgroundColor:state}}>
     //    <h2>{state}</h2>
     //    <button onClick={fun1}>click</button>
     //    <Home/>
     //    </div>
+    // <div>
+    //   <Home/>
+    // </div>
+
+    // <div>
+    //   <hello/>
+    // </div>
+
+  //)
+
+
+//} //rafce
+
+// 06 october
+
+// import React,{useState}from 'react'
+
+// const App = () =>{
+//     let [input,SetInput]=useState("")
+//     let[data,SetData]=useState([])
+//     function fun1(e){
+//         SetInput(e.target.value)
+//     }
+//     function done(){
+//       SetData([...data,input])
+//     }
+
+//     function d(id){
+//       let filterData=data.filter((a,b)=>{
+//         return b!=id
+//       }) 
+//       SetData(filterData)
+//     }
+
+//     return(
+//         <div>
+//            {/* <h2>{input}</h2>  */}
+//            <input onChange={fun1}/>
+//            <button onClick={done}>click</button>
+//            {/* <button onClick={done}>delete</button> */}
+//            {
+//             data.map((a,index)=>{
+//               return(<>
+//               <h2>{a}</h2>
+//               <button onClick={()=>d(index)}>delete</button>
+//               </>)
+//             })
+//            }
+//       </div>
+//     )
+// }
+
+// export default App
+
+import React, {useState} from 'react'
+
+const App = () => {
+  let [input,SetInput]=   useState({
+      name:"",
+      email:"",
+      passWord:""
+    })
+
+     function fun1(e){
+      let  {name,value}=    e.target
+      SetInput({...input,[name]:value})
+      console.log(input);
+      
+      // console.log(e.target);
+    }
+
+  return (
     <div>
-      <Home/>
+      {/* <h2>{input}</h2> */}
+      <input   name='name' value ={input.name} onChange={fun1} type='text' placeholder='Name'/>
+      <br></br>
+      <br></br>
+      <input  name='passWord' value={input.passWord}  onChange={fun1} type='text' placeholder='passWord'/>
+      <br></br>
+      <input  type='email' name="email" onChange={fun1}  value={input.email}/>
+
     </div>
-
   )
-
-} //rafce
+}
 export default App
 
